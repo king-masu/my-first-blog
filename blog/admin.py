a59af9571998234a649683379f5353b35c49270d
masu <kingmasu7@gmail.com>
2019-12-09 14:27:59 +0100

    added edit, new post, small restriction

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('published_date', 'author')
+    search_fields = ('title', 'author', 'text')
+    date_hierarchy = ('published_date')
+    order = ('published_date')
